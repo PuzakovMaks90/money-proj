9596fea9718805a17ef2ed9c7958c53c230c2543
Puzakov Maxym <m.puzakov90@gmail.com>
2022-09-17 18:04:51 +0300

    add contributeMoney

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ let balance = 1000;
 let execute = true;
 
 while (execute) {
-    let choice = prompt('1. Снять деньги; 2. Баланс; 3. Выход');
+    let choice = prompt('1. Снять деньги; 2. Баланс; 3. Внести деньги; 4. Выход');
     if (choice == '1') {
         getMoney();
         getBalance();
     } else if (choice == '2') {
         getBalance();
     } else if (choice == '3') {
+        contributeMoney();
+        getBalance();
+    } else if (choice == '4') {
         execute = false;
         userExit();
     }
 }
+
 
 function getMoney() {
     let summ = Number(prompt('Введите сумму для снятия'));
@@ -21,6 +25,13 @@ function getMoney() {
 function getBalance() {
     alert('Баланс ' + balance);
 }
+
+function contributeMoney() {
+    let summ = Number(prompt('Введите сумму для внесения'));
+    balance = balance + summ;
+    alert('Спасибо на внесенные деньги!');
+}
+
 function userExit() {
     alert('Выполнение программы завершено!');
 }
