33bcaa14561987db5ec5422dc79202d69db78284
Puzakov Maxym <m.puzakov90@gmail.com>
2022-09-17 23:19:32 +0300

    complete

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
 let balance = 1000;
-let execute = true;
+let limit = 5000;
+let limitDay = 6000;
 
-while (execute) {
-    let choice = prompt('1. Снять деньги; 2. Баланс; 3. Внести деньги; 4. Выход');
+while (true) {
+    let choice = prompt('1. Снять деньги; 2. Внести деньги 3. Баланс; 4. Выход');
+
     if (choice == '1') {
         getMoney();
         getBalance();
     } else if (choice == '2') {
+        putMoney();
         getBalance();
     } else if (choice == '3') {
-        contributeMoney();
         getBalance();
     } else if (choice == '4') {
-        execute = false;
         userExit();
+        break;
     }
 }
 
-
 function getMoney() {
     let summ = Number(prompt('Введите сумму для снятия'));
-    balance = balance - summ;
-}
-function getBalance() {
-    alert('Баланс ' + balance);
+    if (summ <= balance){
+        alert('Вы снимаете: ' + summ);
+        balance -= summ;
+    } else if (summ > balance) {
+        alert('Недостаточно средств')
+    }
 }
 
-function contributeMoney() {
-    let summ = Number(prompt('Введите сумму для внесения'));
-    balance = balance + summ;
-    alert('Спасибо на внесенные деньги!');
+function putMoney() {
+    let summ = Number(prompt('Введите сумму для внесение'));
+    if (summ < limit ) {
+        alert('Вы вносите: ' + summ);
+        balance += summ;
+    } else if (summ >= limit) {
+        alert('Сумма взноса не может быть больше: ' + limit );
+    } 
+}
+
+function getBalance() {  
+    if (balance < limitDay) {    
+    alert('Баланс: ' + balance);
+    } else if (balance >= limitDay) {    
+        alert('В день сумма взноса не может быть больше: ' + limit );
+        }
 }
 
 function userExit() {
-    alert('Выполнение программы завершено!');
+    alert('Выход');
 }
